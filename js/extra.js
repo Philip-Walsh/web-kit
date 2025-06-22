508dd25735bcdc84893fa919ebd6c53b74dab0bb
Philip Walsh <59885325+Philip-Walsh@users.noreply.github.com>
2025-06-22 19:31:36 +0000

    add scripts

--- js/extra.js
+++ js/extra.js
@@ -3,9 +3,4 @@ console.log("🚀 extra.js loaded successfully!");
 export function run() {
   alert("🚀 extra.js loaded and run!");
   console.log("extra.js run() executed");
-
-  function showAlert(message = "Extra JS is alive!") {
-    alert(message);
-  }
-
 }
